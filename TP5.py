f33bb23df0daa14e1a192e7ce6790a492b888065
Leo Lamy <leo.lamy@etu.utc.fr>
2023-05-15 12:42:46 +0000

    Update TP5.py

--- TP5.py
+++ TP5.py
@@ -18,31 +18,31 @@ class Fenetre(Tk):
         self.config(bg='black')
         self.eval('tk::PlaceWindow . center')
         self.str_operation = "" # str qui permet rStocker les opérations
-        Label(self,text="Calculatrice d'Alexandre et Léo",fg='white',bg='black').grid(row=0, column=3, padx=10,pady=15)
+        Label(self,text="Calculatrice d'Alexandre et Léo",fg='white',bg='black').grid(row=0,column=3,padx=10,pady=10)
         self.operation = StringVar()
         self.operation.set('')
         self.build()
 
     def build(self):
-        parametre_bouton_nombre = {"bd" : '10', "bg" : 'white', "fg" : 'black'}
-        parametre_bouton_calcul = {"bd" : '10', "bg" : 'yellow', "fg" : 'black'}
-        parametre_bouton_clear = {"bd" : '10',"bg": 'green',"fg" : 'black'}
-        parametre_bouton_egal = {"bd" : '10',"bg": 'red',"fg" : 'black'}
+        parametre_bouton_nombre = {'bd' : '10', 'bg' : 'white', 'fg' : 'black'}
+        parametre_bouton_calcul = {'bd' : '10', 'bg' : 'yellow', 'fg' : 'black'}
+        parametre_bouton_clear = {'bd': '10','bg': 'green','fg' : 'black'}
+        parametre_bouton_egal = {'bd' : '10','bg': 'red','fg' : 'black'}
 
         frame = Frame(self)
         frame.grid()
         #Nombre
-        Button(frame,parametre_bouton_nombre, text='1', command=lambda: self.ajouter('1')).grid(row=2, column=0, padx=5, pady=10)
-        Button(frame, parametre_bouton_nombre, text='2', command=lambda: self.ajouter('2')).grid(row=2, column=1, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='3', command=lambda: self.ajouter('3')).grid(row=2, column=2, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='4', command=lambda: self.ajouter('4')).grid(row=3, column=0, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='5', command=lambda: self.ajouter('5')).grid(row=3, column=1, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='6', command=lambda: self.ajouter('6')).grid(row=3, column=2, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='7', command=lambda: self.ajouter('7')).grid(row=4, column=0, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='8', command=lambda: self.ajouter('8')).grid(row=4, column=1, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='9', command=lambda: self.ajouter('9')).grid(row=4, column=2, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='0', command=lambda: self.ajouter('0')).grid(row=5, column=1, padx=5,pady=10)
-        Button(frame, parametre_bouton_nombre, text='.', command=lambda:self.ajouter('.')).grid(row=5, column=2 , padx=5,pady=10)
+        Button(frame,parametre_bouton_nombre, text='1', command=lambda: self.ajouter('1')).grid(row=3, column=0, padx=5, pady=10)
+        Button(frame, parametre_bouton_nombre, text='2', command=lambda: self.ajouter('2')).grid(row=3, column=1, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='3', command=lambda: self.ajouter('3')).grid(row=3, column=2, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='4', command=lambda: self.ajouter('4')).grid(row=4, column=0, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='5', command=lambda: self.ajouter('5')).grid(row=4, column=1, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='6', command=lambda: self.ajouter('6')).grid(row=4, column=2, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='7', command=lambda: self.ajouter('7')).grid(row=5, column=0, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='8', command=lambda: self.ajouter('8')).grid(row=5, column=1, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='9', command=lambda: self.ajouter('9')).grid(row=5, column=2, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='0', command=lambda: self.ajouter('0')).grid(row=6, column=1, padx=5,pady=10)
+        Button(frame, parametre_bouton_nombre, text='.', command=lambda:self.ajouter('.')).grid(row=6, column=2, padx=5,pady=10)
 
 
         #Boutons de calcul
@@ -58,32 +58,34 @@ class Fenetre(Tk):
         Button(frame,parametre_bouton_calcul, text='π', command=lambda: self.ajouter(str(round(pi,4)))).grid(row=6, column=4, padx=5, pady=10)
 
         #Bouton clear et egal
-        Button(frame,parametre_bouton_clear, text='C', command=lambda: self.clear()).grid(row=2, column=4, padx=5, pady=10)
+        Button(frame,parametre_bouton_clear, text='C', command=lambda: self.clear()).grid(row=2, column=0, padx=5, pady=10)
         Button(frame,parametre_bouton_egal,text = '=', command=lambda : self.calculer()).grid(row = 6, column = 0,padx=5, pady=10)
 
         #Parenthèses
-        Button(frame,parametre_bouton_egal,text = '(', command=lambda : self.ajouter('(')).grid(row = 6, column = 1,padx=5, pady=10)
-        Button(frame,parametre_bouton_egal,text = ')', command=lambda : self.ajouter(')')).grid(row = 6, column = 2,padx=5, pady=10)
+        Button(frame,parametre_bouton_egal,text = '(', command=lambda : self.ajouter('(')).grid(row = 2, column = 1,padx=5, pady=10)
+        Button(frame,parametre_bouton_egal,text = ')', command=lambda : self.ajouter(')')).grid(row = 2, column = 2,padx=5, pady=10)
 
         #Fleches
-        Button(frame,parametre_bouton_egal,text = '←', command=lambda : self.supp()).grid(row = 2, column = 6,rowspan=3,padx=5, pady=10)
-        Button(frame,parametre_bouton_egal,text = '↑', command=lambda : self.fleche('↑')).grid(row = 3, column = 6,rowspan=3,padx=5, pady=10)
-        Button(frame,parametre_bouton_egal,text = '↓', command=lambda : self.fleche('↓')).grid(row = 4, column = 6,rowspan=3,padx=5, pady=10)
+        Button(frame,parametre_bouton_egal,text = '←', command=lambda : self.supp()).grid(row = 3, column = 6,padx=5, pady=10)
+        Button(frame,parametre_bouton_egal,text = '↑', command=lambda : self.fleche('↑')).grid(row = 4, column = 6,padx=5, pady=10)
+        Button(frame,parametre_bouton_egal,text = '↓', command=lambda : self.fleche('↓')).grid(row = 5, column = 6,padx=5, pady=10)
     def ajouter(self,car):
         Fenetre.nb = 0
         self.operation += str(car)
         self.operation.set(self.str_operation)
 
-   # def calculer(self):
+    '''def calculer(self):
+        try:
+            calcul = self.str_operation.replace('x²','**2').replace('√x','sqrt')
 
 
-   # def clear(self):
+    def clear(self):
 
 
-   # def fleche(self,sens):
+    def fleche(self,sens):
 
 
-   # def supp(self):
+    def supp(self):'''
 
 
 

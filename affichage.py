ffdceb7e80a7115cc17542553bf45c851db60048
Alexandre Eberhardt <alexandre.eberhardt@etu.utc.fr>
2023-06-17 14:40:29 +0000

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -1,9 +1,10 @@
 import sqlite3
+
 def get_logements(connexion, curseur):
     nom = input("Entrez le nom du logeur: ")
     prenom = input("Entrez le prénom du logeur: ")
 
-    # on recherche l'id_logeur en considerant qu'il n'y a pas d'homonymes, a
+    # on recherche l'id_logeur en considerant qu'il n'y a pas d'homonymes
     curseur.execute('SELECT id_logeur FROM logeur WHERE nom = ? AND prenom = ?', (nom, prenom))
     result = curseur.fetchone()
 
@@ -11,27 +12,42 @@ def get_logements(connexion, curseur):
     if result is not None:
         id_logeur = result[0]
 
-        # Recherchons les logements de ce logeur
-        curseur.execute('SELECT numero_rue, nom_rue, code_postal, ville, type FROM logement WHERE id_logeur = ?', (id_logeur,))
+        # on recherche les logements de ce logeur
+        curseur.execute('SELECT id_logement, numero_rue, nom_rue, code_postal, ville, type FROM logement WHERE id_logeur = ?', (id_logeur,))
         logements = curseur.fetchall()
 
         # S'il existe des logements pour ce logeur, on affiche tout
         if logements:
             print(f"Nom du logeur : {prenom} {nom}")
-            # Affichons les logements
-            for i, logement in enumerate(logements, start=1):
-                print(f"Logement {i} : {logement[0]} {logement[1]} {logement[2]} {logement[3]} *** {logement[4]}")
+            num_logement=1
+            # on affiche les logements
+            for logement in logements:
+                print(f"Logement {num_logement} : \n{logement[1]} {logement[2]} {logement[3]} {logement[4]} {logement[5]}")
+                num_logement+=1
+                # on recherche les étudiants dans ce logement
+                curseur.execute('SELECT nom, prenom FROM etudiant WHERE id_logement = ?', (logement[0],))
+                etudiants = curseur.fetchall()
+
+                # Si il y a des étudiants dans ce logement, alors on peut les afficher comme selon le modele
+                if etudiants:
+                    for etudiant in etudiants:
+                        print(f"Nom de l'étudiant : {etudiant[0]} {etudiant[1]}")
+                #sinon on affiche qu'il n'y en a pas
+                else:
+                    print("Il n'y a pas d'étudiants dans ce logement")
 
         else:
-            print("Le logeur existe et n'a pas de logements")
+            print("Le logeur existe mais n'a pas de logements")
     else:
         print(f"le logeur nommé {prenom} {nom} n'est pas dans la table")
         #on peut imaginer une fonction qui demanderai a l'utilisateur si iel veut ajouter le logeur dans la base de donnée
         #il faudrait demander les informations et l'ajouter avec la methode du fichier "remplissage.py"
     curseur.close()
+
 def main():
     connexion = sqlite3.connect("alesc.sqlite")
     curseur = connexion.cursor()
     get_logements(connexion, curseur)
+
 if __name__ == '__main__':
     main()
